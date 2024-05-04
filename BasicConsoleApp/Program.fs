b10b595365c5f9155d76252292cb769847ca15be
Nameless 9000 <dev@name1ess.ga>
2024-05-04 21:33:31 +0100

    Recursive join function

--- BasicConsoleApp/Program.fs
+++ BasicConsoleApp/Program.fs
@@ -1,2 +1,10 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module IntegersAndNumbers =
+    let sampleTableOfSquares = [ for i in 0 .. 99 -> "hi: " + i.ToString() ]
+
+    let rec join (x: list<string>) (seperator: string): string =
+        if x.IsEmpty then
+            ""
+        else
+            x.Head + seperator + join x.Tail seperator
+
+    printfn "%s" (join sampleTableOfSquares "\n")
