7604da1342d5a304a00ca3da5a74c38dc13195f1
Nameless 9000 <dev@name1ess.ga>
2024-05-05 01:22:50 +0100

    More operations

--- CalculatorApp/Program.fs
+++ CalculatorApp/Program.fs
@@ -1,32 +1,49 @@
 ﻿open System.Text.RegularExpressions
 
 module Calculator =
-    type BinOp = Add | Sub
+    type BinOp = Add | Sub | Mul | Div | Rem | Exp | Xor | Or | And
 
     type Expr =
-        | Number of int
+        | Number of float
         | BinaryOp of BinOp * Expr * Expr
 
     let rec eval = function
         | Number n -> n
         | BinaryOp (op, e1, e2) ->
-            let v1 = eval e1
-            let v2 = eval e2
+            let f1 = eval e1 in
+            let f2 = eval e2 in
+            let i1 = int (System.Math.Round(f1)) in
+            let i2 = int (System.Math.Round(f2)) in
+
             match op with
-            | Add -> v1 + v2
-            | Sub -> v1 - v2
+            | Add -> f1 + f2
+            | Sub -> f1 - f2
+            | Mul -> f1 * f2
+            | Div -> f1 / f2
+            | Rem -> f1 % f2
+            | Exp -> f1 ** f2
+            | Xor -> int (i1 ^^^ i2)
+            | Or -> int (i1 ||| i2)
+            | And -> int (i1 &&& i2)
 
     let rec parse_expr (tokens: list<string>) =
         let rec inner (acc: Expr) (tokens: list<string>) =
             match tokens with
             | [] -> acc
             | num :: _ when System.Int32.TryParse(num).Equals(true) ->
-                Number(int num)
+                Number(float num)
             | symbol :: tail ->
                 let op =
                     match symbol with
                     | "+" -> Add
                     | "-" -> Sub
+                    | "*" -> Mul
+                    | "/" -> Div
+                    | "%" -> Rem
+                    | "**" -> Exp
+                    | "^" -> Xor
+                    | "|" -> Or
+                    | "&" -> And
                     | _ -> failwith "Unknown operator"
 
                 BinaryOp(op, acc, parse_expr tail)
@@ -34,11 +51,11 @@ module Calculator =
         if tokens.Length = 0 then
             failwith "Unexpected end of input"
 
-        inner (Number(int (List.head tokens))) (List.tail tokens)
+        inner (Number(float (List.head tokens))) (List.tail tokens)
 
 
     let tokenize (input: string) =
-        let matches = Regex.Matches(input, "(\d+)|([+-])")
+        let matches = Regex.Matches(input, "(\d+)|([+\-*/^|&]+)")
         [ for m in matches -> m.Value ]
 
     let parse (input: string) =
@@ -47,7 +64,7 @@ module Calculator =
         parse_expr tokens
 
     [<EntryPoint>]
-    let rec main args =
+    let rec main _ =
         try
             printf "Enter the calculation: "
             let calculation = System.Console.ReadLine()
@@ -56,6 +73,6 @@ module Calculator =
 
             printfn "%A\n" result
         with
-            | ex -> printfn "Error: %s" ex.Message
+            | ex -> printfn "Error: %s\n" ex.Message
 
         main [||]
